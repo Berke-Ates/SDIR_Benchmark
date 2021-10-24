4af641615421b55a415a51e1e18032a5d2158016
Berke Ates <beates@student.ethz.ch>
2021-10-24 20:01:31 +0200

    added sudo + source

--- run.sh
+++ run.sh
@@ -4,12 +4,18 @@
 
 cd $(dirname $0) # navigate to the folder of this script
 
+if [[ $UID != 0 ]]; then
+    echo "Please run this script with sudo:"
+    echo "sudo $0 $*"
+    exit 1
+fi
+
 chmod -R +x scripts/ # enable execution of all scripts
 
 mkdir -p logs
 mkdir -p gen
 
-scripts/dependencies.sh
+source scripts/dependencies.sh
 scripts/test_c.sh 
 scripts/test_affine_for.sh 
 scripts/test_affine_parallel.sh 
