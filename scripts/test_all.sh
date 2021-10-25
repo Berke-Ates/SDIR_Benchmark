338d24daae2c05788fde16519bd1b8a46075dfd2
Berke Ates <beates@student.ethz.ch>
2021-10-25 22:08:36 +0200

    errata

--- scripts/test_all.sh
+++ scripts/test_all.sh
@@ -3,7 +3,7 @@
 # This script runs the all benchmarks
 
 cd $(dirname $0) # navigate to this folder
-./test_c
-./test_affine_for
-./test_affine_parallel
-./test_sdir
+./test_c.sh
+./test_affine_for.sh
+./test_affine_parallel.sh
+./test_sdir.sh
