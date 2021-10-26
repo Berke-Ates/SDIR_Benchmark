e1aff15ca416b7d737bd175faa63b0cceb170994
Berke Ates <beates@student.ethz.ch>
2021-10-26 11:38:31 +0200

    removed variance

--- scripts/eval_logs.py
+++ scripts/eval_logs.py
@@ -9,6 +9,5 @@ print("Min: ", str(dt.min()[0]))
 print("Max: ", str(dt.max()[0]))
 print("Mean: ", str(dt.mean()[0]))
 print("Median: ", str(dt.median()[0]))
-print("Variance: ", str(dt.var()[0]))
 print("STD: ", str(dt.std()[0]))
 
